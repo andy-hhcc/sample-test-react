ee725d70d97fffbc36e4f1dbc8e183d712a9254c
Ka Nguyen <khoaanh2212@gmail.com>
2017-06-05 22:41:12 +0700

    date picker for dashboard

--- src/components/Dashboard/DateRange.jsx
+++ src/components/Dashboard/DateRange.jsx
@@ -1,31 +1,125 @@
 import React, {Component} from 'react';
+import {EasyCalendar} from '../DatePickerField/EasyDatePicker.component';
+import moment from 'moment';
 
+
+let formattedToday = () => {
+  let today = new Date();
+  let day = today.getDate();
+  let month = today.getMonth() + 1; // January is 0
+  let year = today.getFullYear();
+
+  if (day < 10) {
+    day = '0' + day
+  }
+  if (month < 10) {
+    month = '0' + month
+  }
+  return month + '/' + day + '/' + year;
+};
 
 export default class DateRange extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showStartDatePicker: false,
+      showEndDatePicker: false,
+      startDate: formattedToday(),
+      endDate: formattedToday(),
+    }
+  }
+
+  changeStartDate(value) {
+    this.setState({
+      startDate: moment(value).format('MM/DD/YYYY'),
+      showStartDatePicker: false
+    });
+  }
+
+  changeEndDate(value) {
+    this.setState({
+      endDate: moment(value).format('MM/DD/YYYY'),
+      showEndDatePicker: false
+    });
+  }
+
+  showStartDatePicker() {
+    this.setState({
+      showStartDatePicker: !this.state.showStartDatePicker,
+      showEndDatePicker: false
+    })
+  }
+
+  showEndDatePicker() {
+    this.setState({
+      showEndDatePicker: !this.state.showEndDatePicker,
+      showStartDatePicker: false
+    })
   }
 
   render() {
-    const {dateRangeData} = this.props;
+    const {startDate, endDate, showStartDatePicker, showEndDatePicker} = this.state;
+
+    let format = 'MM/DD/YYYY';
+
+
+    let theme = {
+      Calendar: {
+        width: "300px",
+        position: "absolute",
+        top: "55px",
+        left: "0",
+        zIndex: "9999",
+        border: "1px solid #ccc"
+      },
+      Day: {fontSize: "15px", fontWeight: "300"},
+      DaySelected: {background: "#00a4ff", color: "#fff"},
+      DayInRange: {background: "#f6f6f6", color: "#000"},
+      DayHover: {
+        background: "#ffffff",
+        color: "#7f8c8d",
+        transform: "scale(1.1) translateY(-10%)",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.4)"
+      },
+      MonthAndYear: {color: "#535353", fontSize: "18px", fontWeight: "700", lineHeight: "18px"},
+      MonthButton: {background: "#fff"}
+    }
+
+    let dateStartProps = {
+      style: {display: showStartDatePicker ? "block" : "none"},
+      date: formattedToday(),
+      onChange: ::this.changeStartDate,
+      format: format,
+      theme: theme
+    };
+
+    let dateEndProps = {
+      style: {display: showEndDatePicker ? "block" : "none"},
+      date: formattedToday(),
+      onChange: ::this.changeEndDate,
+      format: format,
+      theme: theme
+    };
+
     return (
       <div className="date-range">
         <div className="from">
           <div>From</div>
           <div className="range-content">
-            <input type="text" value={dateRangeData.start} className="form-control"/>
-            <div className="square-grey inline-block"></div>
+            <input type="text" value={startDate} className="form-control" onClick={::this.showStartDatePicker}/>
+            <div className="square-grey inline-block" onClick={::this.showStartDatePicker}></div>
           </div>
+          <EasyCalendar {...dateStartProps}/>
 
         </div>
         <div className="to">
           <div>To</div>
           <div className="range-content">
-            <input type="text" value={dateRangeData.end} className="form-control"/>
-            <div className="square-grey inline-block"></div>
+            <input type="text" value={endDate} className="form-control" onClick={::this.showEndDatePicker}/>
+            <div className="square-grey inline-block" onClick={::this.showEndDatePicker}></div>
           </div>
+          <EasyCalendar {...dateEndProps}/>
         </div>
-
       </div>
     );
   }
--- src/containers/DashboardPage/General/index.js
+++ src/containers/DashboardPage/General/index.js
@@ -11,7 +11,12 @@ const data = [
   {name: '04/03', uv: 239, pv: 380},
   {name: '06/03', uv: 149, pv: 430},
   {name: '07/03', uv: 349, pv: 230},
-  {name: '08/03', uv: 149, pv: 330},
+  {name: '08/03', uv: 149, pv: 370},
+  {name: '09/03', uv: 159, pv: 339},
+  {name: '10/03', uv: 179, pv: 322},
+  {name: '11/03', uv: 119, pv: 110},
+  {name: '12/03', uv: 109, pv: 239},
+  {name: '13/03', uv: 49, pv: 432},
 ];
 export default class General extends React.Component {
   constructor(props) {
